bdfddd0bf0987895025bae8df7ce571d55419aa1
testingQA2 <144715118+testingQA2@users.noreply.github.com>
2023-09-30 20:16:14 +0300

    Дни недели

--- task03/Program.cs
+++ task03/Program.cs
@@ -3,3 +3,21 @@
 // 3 -> Среда
 // 5 -> Пятница
 
+Console.WriteLine("Введите день недели:");
+string? weekDay = Console.ReadLine();
+if (weekDay == "1")
+    Console.WriteLine($"{weekDay} -> Понедельник");
+else if (weekDay == "2")
+    Console.WriteLine($"{weekDay} -> Вторник");
+else if (weekDay == "3")
+    Console.WriteLine($"{weekDay} -> Среда");
+else if (weekDay == "4")
+    Console.WriteLine($"{weekDay} -> Четверг");
+else if (weekDay == "5")
+    Console.WriteLine($"{weekDay} -> Пятница");
+else if (weekDay == "6")
+    Console.WriteLine($"{weekDay} -> Суббота");
+else if (weekDay == "7")
+    Console.WriteLine($"{weekDay} -> Воскресенье");
+else
+    Console.WriteLine("Некорректный ввод");
