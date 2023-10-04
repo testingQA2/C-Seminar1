df255b06f61c9ce871489ca21aceaa1b70883648
testingQA2 <144715118+testingQA2@users.noreply.github.com>
2023-10-04 23:55:36 +0300

    Познакомились с тернарным оператром

--- task09/Program.cs
+++ task09/Program.cs
@@ -9,8 +9,22 @@
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка [10, 99] -> {number}");
 
-int firstDigit = number / 10; // 78 / 10 = 7.8 = 7
-int secondDigit = number % 10;
+// int firstDigit = number / 10; // 78 / 10 = 7.8 = 7
+// int secondDigit = number % 10;
 
-if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
-else Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
+// if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");
+
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit; // тернарный оператор
+// Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+int MaxDigit(int num) //метод
+{
+    int firstDigit = num / 10; // 78 / 10 = 7.8 = 7
+    int secondDigit = num % 10;
+    int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
