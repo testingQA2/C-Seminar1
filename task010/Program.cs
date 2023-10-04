cf4675ceb72ef5f01edab425b6868e916089a82e
testingQA2 <144715118+testingQA2@users.noreply.github.com>
2023-10-05 00:37:23 +0300

    Вывод из трехзначного числа первого и третьего

---
+++ task010/Program.cs
@@ -0,0 +1,30 @@
+﻿// 11. Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает вторую
+// цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+
+int number = new Random().Next(100, 1000);
+
+int deleteSecondDigit = DeleteSecondDigit(number);
+Console.WriteLine($"{number} -> {deleteSecondDigit}");
+
+int DeleteSecondDigit(int num) //решение с помощью метода
+{
+    int firstDigit = num / 100; 
+    int lastDigit = num % 10;
+    int numResult = firstDigit * 10 + lastDigit;
+    return numResult;
+}
+
+
+// int number = new Random().Next(100, 1000); Решение задачи способом написания кода
+// int firstDigit = number / 100; 
+// int lastDigit = number % 10;
+// int numResult = firstDigit * 10 + lastDigit;
+// Console.WriteLine($"{number} -> {numResult}");
+
+
+
