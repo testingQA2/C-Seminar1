77b5c7d162b8f82df75a8238b9e27bccfa17cc4e
testingQA2 <144715118+testingQA2@users.noreply.github.com>
2023-10-05 01:19:08 +0300

    Показывает третью цифру

---
+++ task013DZ/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 13: Напишите программу, которая выводит
+// третью цифру заданного числа или сообщает, что третьей
+// цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.Write("Введите число: "); // Ввод значения пользователем
+string ? number = Console.ReadLine();
+
+char[] numberArray = number.ToCharArray();
+if (numberArray.Length >= 3)
+{
+    Console.Write($"{number} -> {numberArray[2]}");
+}
+else
+{
+    Console.Write($"{number} -> третьей цифры нет");
+}
